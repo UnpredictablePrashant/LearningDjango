8e12eadce2d51f784058dcf2388d9906527f5f57
Prashant Dey <prashantsavior@gmail.com>
2021-12-09 09:05:58 +0530

    FIX: fixing admin view

--- blog/blogpage/urls.py
+++ blog/blogpage/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from django.urls.resolvers import URLPattern
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.helloWorld, name='helloWorld'),
     path('page', views.index, name='index'),
     path('register', views.register, name='register'),
     path('login', views.signin, name='signin'),
     path('profile', views.userProfile, name='userProfile'),
     path('logout', views.userLogout, name='userLogout')
-}
+]
